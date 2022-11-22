2901563e56794553308d59d6d540cfa664ae54fc
bBraian <braianvoficial@gmail.com>
2022-11-21 22:08:04 -0300

    stripe key

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+export const stripe = new Stripe('sk_test_51M4v0WB25TU3K2wx2BVAg1rtptgf2txUOyj7QMggvmSYTYrpdp4BeVSdXupUX5bLilRS3eCU0WuB9jOG0ncJT7Yz00MPYUytUw', {
     apiVersion: '2022-11-15',
     appInfo: {
         name: 'Ignite Shop'
